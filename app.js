4b3c2cb3ac64da70492498a32e5596bb34535205
Ty Le <anhtuan.n.le@gmail.com>
2017-01-20 17:30:01 -0500

    Added Mongo DB and Mongoose

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express 	= require("express"),
+	app 		= express(),
+	bodyParser  = require("body-parser"),
+	mongoose	= require("mongoose")
 
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+
+// SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create( 
+// {
+// 	name: "Weeping Morning", 
+// 	image: "https://farm6.staticflickr.com/5108/5789045796_27c9217bf2.jpg"
+
+// }, function(err, campground){
+// 	if (err){
+// 		console.log(err);
+// 	} else {
+// 		console.log("==== NEW CAMPGROUND CREATED ====")
+// 		console.log(campground);
+// 	}
+// });
+
 
 var campgrounds = [
 		{name: "Jenny Lake", image: "https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg"},
@@ -16,7 +42,6 @@ var campgrounds = [
 		{name: "Rusty Rule Lake", image: "https://farm5.staticflickr.com/4100/4798314980_bc31231984.jpg"},
 		{name: "Apex Mountain", image: "https://farm9.staticflickr.com/8002/7299820870_e78782c078.jpg"},
 		{name: "Fujimoto Hills", image: "https://farm4.staticflickr.com/3832/9603531635_e348167e39.jpg"},
-		{name: "Weeping Morning", image: "https://farm6.staticflickr.com/5108/5789045796_27c9217bf2.jpg"}
 	]
 
 
@@ -25,7 +50,15 @@ app.get("/", function(req, res){
 });
 
 app.get("/campgrounds", function(req, res){
-	res.render("campgrounds", {campgrounds: campgrounds});
+
+	// Get all campgrounds from DB
+	Campground.find({}, function(err, allCampgrounds){
+		if (err) {
+			console.log(err);
+		} else {
+			res.render("campgrounds", {campgrounds: allCampgrounds});
+		}
+	});	
 });
 
 app.post("/campgrounds", function(req, res){
@@ -33,9 +66,15 @@ app.post("/campgrounds", function(req, res){
 	var name = req.body.name;
 	var image = req.body.image;
 	var newCampground = {name: name, image: image}
-	campgrounds.push(newCampground);
-	// redirect back to campgrounds page default is to GET
-	res.redirect("/campgrounds");
+	// Create a new campground and save to database
+	Campground.create(newCampground, function (err, newlyCreated){
+		if (err) {
+			console.log(err);
+		} else {
+			// redirect back to campgrounds page default is to GET
+			res.redirect("/campgrounds")
+		}
+	});
 }); 
 
 app.get("/campgrounds/new", function(req, res){
