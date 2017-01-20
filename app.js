473b3467d18f417112c684fa1690a121e403d881
Ty Le <anhtuan.n.le@gmail.com>
2017-01-19 19:01:49 -0500

    Finished Add Campground page

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+var campgrounds = [
+		{name: "Jenny Lake", image: "https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg"},
+		{name: "MountainView High", image: "https://farm7.staticflickr.com/6105/6381606819_df560e1a51.jpg"},
+		{name: "Mount Desert", image: "https://farm3.staticflickr.com/2116/2164766085_0229ac3f08.jpg"}
+	]
 
 
 app.get("/", function(req, res){
@@ -10,14 +18,22 @@ app.get("/", function(req, res){
 });
 
 app.get("/campgrounds", function(req, res){
-	var campgrounds = [
-		{name: "Jenny Lake", image: "https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg"},
-		{name: "MountainView High", image: "https://farm7.staticflickr.com/6105/6381606819_df560e1a51.jpg"},
-		{name: "Mount Desert", image: "https://farm3.staticflickr.com/2116/2164766085_0229ac3f08.jpg"}
-	]
 	res.render("campgrounds", {campgrounds: campgrounds});
 });
 
+app.post("/campgrounds", function(req, res){
+	//get data from form and ad to campgrounds array
+	var name = req.body.name;
+	var image = req.body.image;
+	var newCampground = {name: name, image: image}
+	campgrounds.push(newCampground);
+	// redirect back to campgrounds page default is to GET
+	res.redirect("/campgrounds");
+}); 
+
+app.get("/campgrounds/new", function(req, res){
+	res.render("new")
+});
 
 
 
