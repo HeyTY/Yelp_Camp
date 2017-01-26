dafc9b2eb2996489620d4b863e7de23fcf83403c
Ty Le <anhtuan.n.le@gmail.com>
2017-01-25 19:45:36 -0500

    Added authenticatoin

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-var express 	= require("express"),
-	app 		= express(),
-	bodyParser  = require("body-parser"),
-	mongoose	= require("mongoose"),
-	Campground  = require("./models/campground"),
-	Comment     = require("./models/comment"),
-	seedDB      = require("./seeds")
+var express 		= require("express"),
+	app 			= express(),
+	bodyParser  	= require("body-parser"),
+	mongoose		= require("mongoose"),
+	passport		= require("passport"),
+	LocalStrategy 	= require("passport-local"),
+	Campground  	= require("./models/campground"),
+	Comment     	= require("./models/comment"),
+	User			= require("./models/user"),
+	seedDB      	= require("./seeds")
 
 
 
@@ -13,6 +16,37 @@ app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 seedDB();
+
+
+
+// PASSPORT CONFIG
+app.use(require("express-session")({
+	secret: "Password OneTwoThree",
+	resave: false,
+	saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+app.use(function(req, res, next){
+	res.locals.currentUser = req.user;
+	next();
+});
+
+
+
 
 
 // Campground.create( 
@@ -50,7 +84,6 @@ app.get("/", function(req, res){
 
 // INDEX - Show all campgrounds
 app.get("/campgrounds", function(req, res){
-
 	// Get all campgrounds from DB
 	Campground.find({}, function(err, allCampgrounds){
 		if (err) {
@@ -101,11 +134,11 @@ app.get("/campgrounds/:id", function(req, res){
 
 
 //=====================================
-//		COMMNENTS ROUTES
+//		COMMENTS ROUTES
 //=====================================
 
 // COMMENT FORM - route to the form 
-app.get("/campgrounds/:id/comments/new", function(req, res){
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res){
 	// find campground by id
 	Campground.findById(req.params.id, function(err, campground){
 		if(err) {
@@ -118,7 +151,7 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
 
 
 // COMMENT POST ROUTE
-app.post("/campgrounds/:id/comments", function (req, res) {
+app.post("/campgrounds/:id/comments", isLoggedIn, function (req, res) {
 	// lookup campground using ID
 	Campground.findById(req.params.id, function(err, campground) {
 		if (err) {
@@ -137,14 +170,59 @@ app.post("/campgrounds/:id/comments", function (req, res) {
 		});
 		}
 	});
-
-
-	// connect new comment to campground
-
-	//redirect campground show page
 })
 
+//============
+// AUTH ROUTES
+//============
+	// SHOW SIGN UP FORM
+app.get("/register", function(req, res){
+	res.render("register");
+});
 
+	// POST
+app.post("/register", function(req, res){
+	var newUser = new User({username: req.body.username});
+	User.register(newUser, req.body.password, function(err, user){
+		if (err) {
+			console.log(err);
+			return res.render("register")
+		}
+		passport.authenticate("local")(req, res, function(){
+			res.redirect("/campgrounds");
+		});
+	});
+});
+
+//============
+// LOGIN ROUTES
+//============
+
+	// SHOW LOGIN FORM
+app.get("/login", function (req, res){
+	res.render("login");
+})
+
+app.post("/login", passport.authenticate("local", {
+		successRedirect: "/campgrounds",
+		failureRedirect: "/login"
+	}), 
+	 function (req, res){
+});
+
+	// Logout 
+app.get("/logout", function(req, res){
+	req.logout();
+	res.redirect("/campgrounds");
+});
+
+
+function isLoggedIn(req, res, next){
+		if(req.isAuthenticated()){
+			return next();
+		}
+		res.redirect("/login");
+}
 
 
 app.listen(3000, function(){
