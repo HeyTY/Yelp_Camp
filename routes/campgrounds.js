86b3a31adbd39e7f6be175880cdfa2bf3c0ece7c
Ty Le <anhtuan.n.le@gmail.com>
2017-01-30 21:49:19 -0500

    Added image slider to landing and price to model

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -24,13 +24,14 @@ router.get("/", function(req, res){
 router.post("/", middleware.isLoggedIn, function(req, res){
 	//get data from form and ad to campgrounds array
 	var name 			= req.body.name;
+	var price			= req.body.price;
 	var image 			= req.body.image;
 	var description 	= req.body.description
 	var author 			= {
 			id: req.user._id,
 			username: req.user.username
 	};
-	var newCampground 	= {name: name, image: image, description: description, author: author};
+	var newCampground 	= {name: name, price: price, image: image, description: description, author: author};
 	// Create a new campground and save to database
 	Campground.create(newCampground, function (err, newlyCreated){
 		if (err) {
